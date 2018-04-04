525dc951cbeceee8ddac2d8fb311316bc901c7e8
Tarah Nixon <tarah.sage.nixon@gmail.com>
2018-04-04 22:54:41 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,6 +1,6 @@
 # code the #greeting method here!
 def greeting(name)
-  puts "Welcome! Please enter your name: "
+  puts "Hi! I'm HAL, what's your name?"
   name = gets.strip
   return "Hello #{name}. It's nice to meet you."
 end
